eda98953c151861a471cefe6b27fd4682b077771
Abdullah Rashed <abdullah_rashed.a@icloud.com>
2019-11-06 23:04:51 +0300

    Updates

---
+++ precache-manifest.80eb8f00115f0a9df960583eb8be6180.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa52cd8b825b29253470be9458137ac5",
+    "url": "/bookstore-app/index.html"
+  },
+  {
+    "revision": "f65070e1d2311c58320d",
+    "url": "/bookstore-app/static/css/main.f295305e.chunk.css"
+  },
+  {
+    "revision": "d40bb77778166a0306e7",
+    "url": "/bookstore-app/static/js/2.9f9304c6.chunk.js"
+  },
+  {
+    "revision": "f65070e1d2311c58320d",
+    "url": "/bookstore-app/static/js/main.d12d0d5a.chunk.js"
+  },
+  {
+    "revision": "ffcaacfb516ad7e5e3d4",
+    "url": "/bookstore-app/static/js/runtime-main.e3ce3659.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Books-app/precache-manifest.50dabfc1d0ee0095c6d1d80e5aa42ab2.js"
+  "/bookstore-app/precache-manifest.80eb8f00115f0a9df960583eb8be6180.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Books-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bookstore-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
